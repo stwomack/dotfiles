b2d08698a6e62ca17d64f52d9afb19bea6f94cfd
Steve Womack <stwomack@gmail.com>
2016-03-25 19:00:03 -0500

    Gotta get the serialization right, but otherwise this is a crisp way to get from AMQP to Kafka.

--- workspace-java/authorization-request-worker/src/main/java/pivotal/io/service/InputProcessor.java
+++ workspace-java/authorization-request-worker/src/main/java/pivotal/io/service/InputProcessor.java
@@ -1,21 +1,16 @@
 package pivotal.io.service;
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.handler.annotation.SendTo;
 
-@EnableBinding(Sink.class)
+@EnableBinding(Processor.class)
 public class InputProcessor {
 
-    @StreamListener(Sink.INPUT)
-    public void processMessage(String message) {
-        System.out.println(message);
+    @StreamListener(Processor.INPUT)
+    @SendTo(Processor.OUTPUT)
+    public String processMessage(String message) {
+        return message;
     }
-
 }
